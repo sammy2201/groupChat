adb363c88b53ee2663684d3d119bf40c06abe1cf
bhavanish <bhavanish2000.com>
2021-06-18 00:31:28 +0530

    version 11 img upload added

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const fs = require('fs');
+const path = require('path');
+const multer = require('multer');
+require('dotenv/config');
 
 
 
 const app = express();
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -37,13 +40,33 @@ mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+var upload = multer({ storage: storage });
+
+/////////////////////////////////////////schema////////////////////////
+
 const chatSchema = new mongoose.Schema({
   chat: String,
   name: {
     type: String,
     unique: false
   },
-  time:String
+  time: String,
+  img:
+  {
+      data: Buffer,
+      contentType: String
+  },
+  image:String
+
 });
 
 
@@ -55,17 +78,30 @@ const userSchema = new mongoose.Schema({
 
 });
 
+// const imageSchema = new mongoose.Schema({
+//     name: String,
+//     desc: String,
+//
+// });
 
+//////////////////////////////////////////////////////////////////
 userSchema.plugin(passportLocalMongoose);
+
+//////////////////////////model//////////////////////////////////
 
 const ChatItem = mongoose.model("chatItem", chatSchema);
 const User = new mongoose.model("User", userSchema);
+ // var imgModel= new mongoose.model('Image', imageSchema);
+////////////////////////////////////////////////////////////////
 
 
+
+/////////////////////passport//////////////////////////////////
 passport.use(User.createStrategy());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+///////////////////////////get////////////////////////////////////
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -81,15 +117,15 @@ app.get("/register", function(req, res) {
 });
 
 
-
-
 app.get("/chat", function(req, res) {
   if (req.isAuthenticated()) {
 
     ChatItem.find(function(err, founditems) {
       res.render("chat", {
         newChat: founditems,
+
       });
+
     });
   } else {
     res.redirect("/login");
@@ -101,7 +137,7 @@ app.get("/logout", function(req, res) {
   res.redirect("/");
 });
 
-
+////////////////////////post///////////////////
 app.post("/login", function(req, res) {
 
   const user = new User({
@@ -141,7 +177,7 @@ app.post("/register", function(req, res) {
 
 
 
-app.post("/chat", function(req, res) {
+app.post("/chat", upload.single('image'),function(req, res) {
   const chat = req.body.chat;
   const nameOfUser = req.user.name;
   const date_ob = new Date();
@@ -149,16 +185,29 @@ app.post("/chat", function(req, res) {
   const minutes = date_ob.getMinutes();
   const currentTime = hours + ":" + minutes;
 
-  const someconstant = new ChatItem({
-    chat: chat,
-    name: nameOfUser,
-    time: currentTime
+  if (chat !== undefined) {
+    const someconstant = new ChatItem({
+      chat: chat,
+      name: nameOfUser,
+      time: currentTime,
+      image:false,
+    });
+    someconstant.save();
+  }
+  if (chat === undefined) {
+    const someconstant = new ChatItem({
 
-  });
+      name: nameOfUser,
+      time: currentTime,
+      img: {
+              data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+              contentType: 'image/png'
+            },
+      image:true,
 
-  someconstant.save();
-
-
+    });
+    someconstant.save();
+  }
 
   res.redirect("/chat");
 });
